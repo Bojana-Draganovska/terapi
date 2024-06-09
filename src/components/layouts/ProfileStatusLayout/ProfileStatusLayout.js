ee5412b134200a2c78a458e552499030fd93d9ec
Jovana <jovana.krstevska@students.finki.ukim.mk>
2024-06-09 17:59:32 +0200

    full complete MyProfile

--- src/components/layouts/ProfileStatusLayout/ProfileStatusLayout.js
+++ src/components/layouts/ProfileStatusLayout/ProfileStatusLayout.js
@@ -5,21 +5,19 @@ import Title from "../../ui/Title/Title";
 import ProfileStatusWidget from "../../widgets/ProfileStatusWidget/ProfileStatusWidget";
 import "../ProfileStatusLayout/ProfileStatusLayout.css";
 import statusData from "../../../status.json";
+import Input from "../../ui/Input/Input"
 import { Link } from "react-router-dom";
 
 function ProfileStatusLayout(props) {
     const [selectedTitle, setSelectedTitle] = useState("");
     const [selectedDay, setSelectedDay] = useState(null);
     const [completedDays, setCompletedDays] = useState({});
-    const [popupContent, setPopupContent] = useState(null);
+    const [activePopup, setActivePopup] = useState(null); 
     const categories = ["Анксиозност", "Менаџирање со гнев", "Депресија"];
 
 
-    const handleOpenPopup = (id) => {
-        setPopupContent(id);
-    };
-    const handleClosePopup = () => {
-        setPopupContent(null); 
+    const handleTogglePopup = (id) => {
+        setActivePopup((prevId) => (prevId === id ? null : id));
     };
 
     const handleWidgetClick = (index) => {
@@ -133,33 +131,36 @@ function ProfileStatusLayout(props) {
                         </div>
                     </div>
                     <div className="buttonsProfile">
-                    <Button classname="buttonProfile" content={"Преглед на податоци"} onClick={() => handleOpenPopup(1)} />
-                    <Button classname="buttonProfile" content={"Ажурирај податоци"} onClick={() => handleOpenPopup(2)} />
-                    <Button classname="buttonProfile" content={"Правила и обврски"} onClick={() => handleOpenPopup(3)} />
-                    <Button classname="buttonProfile" content={"Одјава"} onClick={props.handleLogout} />
-                </div>
-                {popupContent===1 && (
-                    <div onClick={() => handleClosePopup()}>
-                        <ProfileStatusWidget style="pregledNaPodatoci1" status={"Електронски маил:"} description={"Лозинка:"}/>
+                        <Button classname="buttonProfile" content={"Преглед на податоци"} onClick={() => handleTogglePopup(1)} />
+                        <Button classname="buttonProfile" content={"Ажурирај податоци"} onClick={() => handleTogglePopup(2)} />
+                        <Button classname="buttonProfile" content={"Правила и обврски"} onClick={() => handleTogglePopup(3)} />
+                        <Button classname="buttonProfile" content={"Одјава"} onClick={props.handleLogout} />
                     </div>
-                    
-                )}
-                {popupContent===2 && (
-                    <div onClick={() => handleClosePopup()}>
-                     <ProfileStatusWidget style="pregledNaPodatoci2" status={"Електронски маил:"} description={"Стара лозинка:"} description1={"Нова лозинка:"}/>
-                     <Button classname="buttonAzuriraj" content={"Ажурирај"}/>
-                    </div>
-                )}
-                {popupContent===3 && (
-                    <div onClick={() => handleClosePopup()}>
-                        <ProfileStatusWidget style="pregledNaPodatoci3" status={"Услови за користење на апликацијата за ментално здравје"} description={"Прифаќање на условите: Со користење на оваа апликација, се согласувате да ги почитувате и да бидете обврзани со овие услови за користење.\n" +
-                        "Политика за приватност: Вашата приватност е важна за нас. Ве молиме прегледајте ја нашата Политика за приватност, која исто така го регулира вашето користење на апликацијата.\n" +
-                        "Користење на апликацијата: Оваа апликација е наменета само за информативни цели и не претставува медицински совет или третман.\n" +
-                        "Одговорности на корисникот: Вие сте одговорни за одржување на доверливоста на вашата сметка и лозинка и за ограничување на пристапот до вашиот уред.\n" +
-                        "Ограничување на одговорност: Провајдерот на апликацијата не е одговорен за каква било директна, индиректна, случајна или последична штета што произлегува од користењето или неможноста за користење на апликацијата.\n" +
-                        "Промени на условите: Го задржуваме  правото да го прекинеме вашиот пристап до апликацијата по наша дискреција, без известување и без одговорност, по која било причина."}/>
-                    </div>
-                )}
+                    {activePopup === 1 && (
+                        <div>
+                            <ProfileStatusWidget style="pregledNaPodatoci1" status={"Електронски маил:"} description={"Лозинка:"} />
+                        </div>
+
+                    )}
+                    {activePopup === 2 && (
+                        <div>
+                             <ProfileStatusWidget style="pregledNaPodatoci2" status={"Електронски маил:"} description={"Стара лозинка:"} description1={"Нова лозинка:"}/>
+                                <Input className="input1" typename={'text'} placeholder={"Внесето го твојот електронски маил:"}></Input>
+                                <Input className="input2" typename={'password'} placeholder={"Внеси ја твојата лозинка"}></Input>
+                                <Input className="input3" typename={'password'} placeholder={"Внеси ја новата лозинка"}></Input>
+                            <Button classname="buttonAzuriraj" content={"Ажурирај"} />
+                        </div>
+                    )}
+                    {activePopup === 3 && (
+                        <div>
+                            <ProfileStatusWidget style="pregledNaPodatoci3" status={"Услови за користење на апликацијата за ментално здравје"} description={"Прифаќање на условите: Со користење на оваа апликација, се согласувате да ги почитувате и да бидете обврзани со овие услови за користење.\n" +
+                                "Политика за приватност: Вашата приватност е важна за нас. Ве молиме прегледајте ја нашата Политика за приватност, која исто така го регулира вашето користење на апликацијата.\n" +
+                                "Користење на апликацијата: Оваа апликација е наменета само за информативни цели и не претставува медицински совет или третман.\n" +
+                                "Одговорности на корисникот: Вие сте одговорни за одржување на доверливоста на вашата сметка и лозинка и за ограничување на пристапот до вашиот уред.\n" +
+                                "Ограничување на одговорност: Провајдерот на апликацијата не е одговорен за каква било директна, индиректна, случајна или последична штета што произлегува од користењето или неможноста за користење на апликацијата.\n" +
+                                "Промени на условите: Го задржуваме  правото да го прекинеме вашиот пристап до апликацијата по наша дискреција, без известување и без одговорност, по која било причина."} />
+                        </div>
+                    )}
                 </>
             )}
         </>
